f1b4968b6366fe79c56f00b401b959c7cc1768f1
JML42 <joshua.mcculloch@sky.uk>
2023-12-14 11:13:25 +0000

    Brings in all component images, adds conditional render to display image

--- src/component/BikeBuilderComponent/DisplayBikeImages.js
+++ src/component/BikeBuilderComponent/DisplayBikeImages.js
@@ -1,5 +1,6 @@
 import { Button } from "react-bootstrap";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const BIKE_IMAGES_API_URL = "http://localhost:8088/demo/Image/";
 const DisplayBikeImages = ({ bike }) => {
@@ -22,41 +23,47 @@ const DisplayBikeImages = ({ bike }) => {
   }
 
   return (
-    <div className="bike-display">
-      <div className="image-container slider">
-        <h1>{listOfImages[imageRef].altText}</h1>
-        <div className="images">
-          <img
-            src={require(`../../images/${listOfImages[imageRef].src}`)}
-            alt={listOfImages[imageRef].altText}
-          />
+    <>
+      {listOfImages.length < 1 ? (
+        <></>
+      ) : (
+        <div className="bike-display">
+          <div className="image-container slider">
+            <h1>{listOfImages[imageRef].altText}</h1>
+            <div className="images">
+              <img
+                src={require(`../../images/${listOfImages[imageRef].src}`)}
+                alt={listOfImages[imageRef].altText}
+              />
+            </div>
+          </div>
+          <div className="controls">
+            <Button
+              onClick={() => {
+                if (imageRef !== 0) {
+                  setImageRef(imageRef - 1);
+                } else {
+                  setImageRef(listOfImages.length - 1);
+                }
+              }}
+            >
+              Back
+            </Button>
+            <Button
+              onClick={() => {
+                if (imageRef === listOfImages.length - 1) {
+                  setImageRef(0);
+                } else {
+                  setImageRef(imageRef + 1);
+                }
+              }}
+            >
+              Next
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="controls">
-        <Button
-          onClick={() => {
-            if (imageRef !== 0) {
-              setImageRef(imageRef - 1);
-            } else {
-              setImageRef(listOfImages.length - 1);
-            }
-          }}
-        >
-          Back
-        </Button>
-        <Button
-          onClick={() => {
-            if (imageRef === listOfImages.length - 1) {
-              setImageRef(0);
-            } else {
-              setImageRef(imageRef + 1);
-            }
-          }}
-        >
-          Next
-        </Button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
