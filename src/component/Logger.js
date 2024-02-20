8422dde67eeef13917afc9d0f8011b8c42a084a6
Josh McCulloch <joshua.mcculloch@sky.uk>
2024-02-20 09:51:23 +0000

    Implements simple Logger from BE

--- src/component/Logger.js
+++ src/component/Logger.js
@@ -3,35 +3,29 @@ import axios from "axios";
 const LOGGER_API = "http://localhost:8088/demo/Test/LogThis";
 
 const Logger = {
-  infoLog: async (message) => {
-    message = "INFO!!" + message;
+  infoLog: async (m) => {
+    const log = { level: "INFO", message: m };
     try {
-      await axios.put(LOGGER_API, {
-        message: message,
-      });
+      await axios.put(LOGGER_API, log);
     } catch (err) {
       console.error(err);
     }
   },
 
-  warnLog: async (message) => {
-    message = "WARN!!" + message;
+  warnLog: async (m) => {
+    const log = { level: "WARN", message: m };
     try {
-      await axios.put(LOGGER_API, {
-        message: message,
-      });
+      await axios.put(LOGGER_API, log);
     } catch (err) {
       console.error(err);
     }
   },
 
-  errorLog: async (message) => {
-    console.error(message)
-    message = "ERROR!!" + message;
+  errorLog: async (m) => {
+    console.error(m);
+    const log = { level: "ERROR", message: m };
     try {
-      await axios.put(LOGGER_API, {
-        message: message,
-      });
+      await axios.put(LOGGER_API, log);
     } catch (err) {
       console.error(err);
     }
