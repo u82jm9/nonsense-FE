e1cf3776dfaa7fc00418e065fe64ed13180002e1
Josh McCulloch <joshua.mcculloch@sky.uk>
2024-04-03 12:55:38 +0100

    Changes Search Back to Bootstrap

--- src/component/WeatherDisplay/WeatherCitySearch.js
+++ src/component/WeatherDisplay/WeatherCitySearch.js
@@ -1,7 +1,7 @@
-import { Box, Input, InputLabel, Button, FormControl } from "@mui/material";
+import { Button } from "@mui/material";
 import React from "react";
 import { useState } from "react";
-// import { Button, Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 const WeatherCitySearch = ({ city, changeCity }) => {
   const [tempCity, setTempCity] = useState("");
@@ -9,40 +9,22 @@ const WeatherCitySearch = ({ city, changeCity }) => {
   return (
     <div className="weather-search">
       <h1>{city}</h1>
-      <Box component="form">
-        <FormControl>
-          <InputLabel>City</InputLabel>
-          <Input
-            type="text"
-            id="city-search"
-            placeholder={city}
-            value={tempCity}
-            onChange={(e) => {
-              console.log("TYPING!!! ", e.target.value);
-              setTempCity(e.target.value);
-            }}
-          />
-        </FormControl>
+      <Form>
+        <Form.Label>Search City</Form.Label>
+        <Form.Control
+          type="text"
+          id="city-search"
+          placeholder={city}
+          value={tempCity}
+          onChange={(e) => {
+            console.log("TYPING!!! ", e.target.value);
+            setTempCity(e.target.value);
+          }}
+        />
         <Button variant="contained" onClick={() => changeCity(tempCity)}>
           Search
         </Button>
-      </Box>
-      {/* <Form>
-        <Form.Group
-          className="weather-city"
-          controlId="city-search"
-          onChange={(e) => {
-            console.log("TYYPIING!!! ", e.target);
-            setTempCity(e.target.value);
-          }}
-        >
-          <Form.Label>Search City</Form.Label>
-          <Form.Control type="text" placeholder={city} />
-        </Form.Group>
-        <Button variant="primary" onClick={() => changeCity(tempCity)}>
-          Search
-        </Button>
-      </Form> */}
+      </Form>
     </div>
   );
 };
